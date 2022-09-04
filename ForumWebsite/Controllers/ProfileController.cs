3a09882c8c0ddb0efba1786388304496cc9d3a78
cemalaydeniz <c.a.aydeniz@gmail.com>
2022-09-04 20:28:55 +0300

    added profile page and its functionality

---
+++ ForumWebsite/Controllers/ProfileController.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using ForumWebsite.Models.Authentication;
+using ForumWebsite.Models.ViewMoels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumWebsite.Controllers
+{
+    [Authorize]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IMapper _mapper;
+
+        public ProfileController(UserManager<User> userManager, SignInManager<User> signInManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            if (User.Identity == null)
+                return View();
+
+            var result = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (result != null)
+            {
+                ProfileViewModel viewModel = new ProfileViewModel()
+                {
+                    Birthday = result.Birthday,
+                    AboutMe = result.AboutMe
+                };
+
+                return View(viewModel);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ProfileViewModel viewModel)
+        {
+            if (User.Identity == null)
+                return View();
+
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(User.Identity.Name);
+                if (user != null)
+                {
+                    user.Birthday = viewModel.Birthday;
+                    user.AboutMe = viewModel.AboutMe;
+
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        await _userManager.UpdateSecurityStampAsync(user);
+                        await _signInManager.SignOutAsync();
+                        await _signInManager.SignInAsync(user, true);
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(error.Code, error.Description);
+                        }
+                    }
+
+                    return View(viewModel);
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ForumWebsite/Models/ViewMoels/ProfileViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumWebsite.Models.ViewMoels
+{
+    public class ProfileViewModel
+    {
+        [Required(ErrorMessage = "Please enter your birthday.")]
+        [DataType(DataType.Date, ErrorMessage = "Please enter a valid date.")]
+        [Display(Name = "Birthday")]
+        public DateTime Birthday { get; set; }
+
+        [DataType(DataType.Text)]
+        [StringLength(255, ErrorMessage = "This section can be the maximum of 255 characters.")]
+        [Display(Name = "About Me")]
+        public string? AboutMe { get; set; }
+    }
+}
